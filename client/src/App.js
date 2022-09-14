b5ebf2f2ffdbb3a2d6e9f756b75d62bb247776a3
gsnorberto <sn_gabriel@outlook.com>
2022-09-14 11:26:36 -0300

    test

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,7 @@ const App = () => {
             <AppBar className={classes.appBar} position="static" color="inherit">
                 <Typography className={classes.heading} variant="h2" align="center">Memorias</Typography>
                 <img className={classes.image} src={memories} alt="memories" height="60" />
+                
             </AppBar>
             <Grow in>
                 <Container>
